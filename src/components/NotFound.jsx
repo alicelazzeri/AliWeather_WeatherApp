8cbd813e2690b9b68a8e9280e66d1a89b22627b6
Alice Lazzeri <alice.lazzeri@hotmail.it>
2023-04-10 19:27:24 +0200

    added style to NotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div id="heroContainer" className="text-center">
       <img src={logo} width={300} alt="Logo" />
       <h1 className="fw-light navTitle mt-1">404 &ndash; Page not found</h1>
-      <p class="lead navTitle">
+      <p class="lead navTitle mb-5">
         We're sorry, but the page you were looking for doesn't seem to exist. Try searching for another page or go back
         to the homepage.
       </p>
